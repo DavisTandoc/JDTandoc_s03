1d89adc24c088ad50eee978266b2f90f9253aa6c
John Dave Tandoc <tandoc2030350@mls.ceu.edu.ph>
2023-03-16 08:19:39 +0800

    includes javascript intro activity

---
+++ index.js
@@ -0,0 +1,18 @@
+let a = 27;
+let b = 9;
+let sum = a + b;
+console.log(`The sum of the numbers is: ${sum}`)
+
+let c = 26;
+let d = 18;
+let difference = c - d;
+console.log(`The difference of the numbers is: ${difference}`)
+
+function multiplyNum(num1, num2){
+	result = num1 * num2;
+	//console.log(result);
+	return result
+}
+
+let product = multiplyNum(2, 9)
+console.log(`The result of the operation is: ${product}`)
